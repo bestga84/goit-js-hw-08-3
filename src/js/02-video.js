0c547cb65c96f581ff28d9555be5ee9c86de5558
bestga84 <bestga84@gmail.com>
2024-01-30 21:15:48 +0200

    video player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframeEl = document.querySelector('#vimeo-player');
+console.log(iframeEl);
+
+const player = new Player(iframeEl);
+console.log(player);
+
+const savedCurrentTime = localStorage.getItem('videoplayer-current-time');
+setSavedCurrentTime();
+
+const currentTimeVideo = data => {
+  console.log(data);
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+
+player.on('timeupdate', throttle(currentTimeVideo, 1000));
+
+function setSavedCurrentTime() {
+  if (savedCurrentTime !== null) {
+    player.setCurrentTime(savedCurrentTime);
+    return;
+  }
+  localStorage.getItem('videoplayer-current-time', '0');
+}
+console.log(localStorage.getItem('videoplayer-current-time'));
